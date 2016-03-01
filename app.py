9a4ffb5a338bb9c5b3ba251a3592589e995942ec
郁骏 <ak64th@gmail.com>
2016-03-02 01:03:06 +0800

    turn on auto decoding of redis-py and add documents about it

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import falcon
 import redis
 from resources import Start
 
+# ### 初始化redis客户端
+# 当decode_responses参数为True时redis-py会自动将redis返回的字符菜转换为unicode。
+# 默认不这么做的原因是：如果需要转换结果为数字，先转换为unicode就造成了额外花销。
+# 节点本身不会从redis读取大量数字，但在归档数据时可能需要注意。
+r = redis.StrictRedis(decode_responses=True)
 
 """
 ### API接口
@@ -16,7 +21,5 @@ from resources import Start
 [资源]: resources.html
 [开始游戏]: resources.html#Start
 """
-r = redis.StrictRedis()
-
 api = falcon.API()
 api.add_route('/api/start/{game_id}', Start(r))
