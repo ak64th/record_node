3e7aa5486974807265f240bdf085d2e6aaa4c6fd
郁骏 <ak64th@gmail.com>
2016-06-08 15:33:34 +0800

    make app configurable

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ import redis
 from resources import Start, End, Answer
 from db import create_engine
 from models import metadata
+from configuration import config
 
 # ### 初始化redis客户端
+redis_config = config['REDIS']
 # 当decode_responses参数为True时redis-py会自动将redis返回的字符菜转换为unicode。
 # 默认不这么做的原因是：如果需要转换结果为数字，先转换为unicode就造成了额外花销。
 # 节点本身不会从redis读取大量数字，但在归档数据时可能需要注意。
-r = redis.StrictRedis(decode_responses=True)
+redis_config.update(decode_responses=True)
+r = redis.StrictRedis(**redis_config)
 
 # ###初始化数据库
-db = create_engine('sqlite:///records.db')
+db = create_engine(config['DATABASE_URL'])
 # 程序在创建表格之前会检测是否存在
 metadata.create_all(db)
 
---
+++ configuration.py
@@ -0,0 +1,8 @@
+config = {
+    'DATABASE_URL': 'sqlite:///records.db',
+    'REDIS': {
+        'host': 'localhost',
+        'port': '6379',
+        'db': 0,
+    },
+}
